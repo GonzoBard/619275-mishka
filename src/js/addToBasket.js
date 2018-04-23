4aade0c331b2c2cc4224c6e7dea20ca8cd53f481
gonzo.bard <gonzo.bard@gmail.com>
2018-04-23 03:49:17 +0300

    modal

--- src/js/addToBasket.js
+++ src/js/addToBasket.js
@@ -1,13 +1,17 @@
 function AddToBasketModal() {
   var modal = document.getElementById('add-to-basket-modal');
   var hideClassName = "visually-hidden";
+  var overflowClassName = "modal-open";
+  var bodyClassList = document.getElementsByTagName('body')[0].classList;
 
   this.open = function (event) {
     event.preventDefault(); //отменить стандартное действие
     modal.classList.remove(hideClassName);
+    bodyClassList.add(overflowClassName);
   };
 
   this.close = function () {
+    bodyClassList.remove(overflowClassName);
     modal.classList.add(hideClassName);
   };
 }
